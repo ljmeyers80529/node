56ad72a726da71c2dc7206f4e169a4adf1c6fb32
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-15 21:45:35 -0700

    Section 4, Lecture 31 -- Encoding user input to construct a complete URL.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,23 @@
 const request = require("request");
+const yargs = require("yargs");
+
 const apiKey = 'AIzaSyA6gAI24ZjS1ODBUaxW1PZDVORkWcG9dbU';
+const mapURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather information for.',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=550%20s%20walling%20ave%20fresno%20ca' + '&key=' + apiKey,
+    url: `${mapURL}${encodeURIComponent(argv.a)}&key=${apiKey}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
