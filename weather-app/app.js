54fa3e7a611507cf21f05492e70c67a2f174516a
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-15 19:26:40 -0700

    Section 4, Lecture 30 -- HTTP Request.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,11 @@
 const request = require("request");
+const apiKey = 'AIzaSyA6gAI24ZjS1ODBUaxW1PZDVORkWcG9dbU';
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=550%20s%20walling%20ave%20fresno%20ca',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=550%20s%20walling%20ave%20fresno%20ca' + '&key=' + apiKey,
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`longitude: ${body.results[0].geometry.location.lng}`);
 });
