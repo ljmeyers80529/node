c6a490820174ac662ba2133a6a46bd78ae356dd8
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-20 13:44:53 -0700

    Refactored to show the different stages of development.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -4,22 +4,27 @@ const hvs = require('hbs');
 var app = express();
 
 app.set('view engine', 'hbs');
-console.log(__dirname + '\\public');
-app.use(express.static(__dirname + '\\public'));
+app.use(express.static(__dirname + '\\public')); // loalhost:xxxx/help.html
 
-app.get('/', (req, res) => { // req = request, res = response
-    // res.send('<h1>Hello express!</h1>');
-    // res.send({
-    //     name: 'Me',
-    //     other: [
-    //         'Other1',
-    //         'Other2'
-    //     ]
-    // });
+// app.get('/', (req, res) => { // req = request, res = response           // simple output to web page with plain text.
+//     res.send('<h1>Hello express!</h1>');
+// });
+
+// app.get('/', (req, res) => { // req = request, res = response           // send a JSON object to the web page.
+//     res.send({
+//         name: 'Me',
+//         other: [
+//             'Other1',
+//             'Other2'
+//         ]
+//     });
+// });
+
+app.get('/', (req, res) => { // req = request, res = response           // send a templated page to the browser.
     res.render('home.hbs', {
         pageTitle: 'Home page',
         currentYear: new Date().getFullYear(),
-        welcome: 'Welcome to the home page.'
+        welcome: 'Welcome to the home page, this is now sunday.'
     })
 });
 
