dfb24abb3444d034d7d143df6ffb184c8b59e4a3
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-21 20:22:42 -0700

    Section 6, LEcture 57 -- Organize tests using Describe.

--- node-tests/server/server.test.js
+++ node-tests/server/server.test.js
@@ -3,25 +3,32 @@ const expect = require('expect');
 
 var app = require('./server.js').app;
 
-it('Should return "Hello there!" response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        // .expect('Hello there!')
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
+describe('Server', () => {
 
-it('Should return /user page information...', (done) => {
-    request(app)
-        .get('/user')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({ name: 'Me', age: 56 }); // /user being an array, must test for all objhect fields.
-        })
-        .end(done);
+    describe('GET \\', () => {
+        it('Should return "Hello there!" response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                // .expect('Hello there!')
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
+
+    describe('GET \\user', () => {
+        it('Should return /user page information...', (done) => {
+            request(app)
+                .get('/user')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({ name: 'Me', age: 56 }); // /user being an array, must test for all objhect fields.
+                })
+                .end(done);
+        });
+    });
 });
