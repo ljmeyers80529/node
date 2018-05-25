f709d55487ea7327e5e91e1b5aa5a148ceab06c2
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-24 21:53:10 -0700

    Section 7, LEcture 71 - Types, validators & defaults.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -3,11 +3,12 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: { type: String },
-    completed: { type: Boolean },
-    completedAt: { type: Number }
-});
+// var Todo = mongoose.model('Todo', {
+//     text: { type: String, required: true, minLength: 1, trim: true },
+
+//     completed: { type: Boolean, default: false },
+//     completedAt: { type: Number, default: null }
+// });
 
 // var newTodo = new Todo({
 //     text: 'Cook dinner'
@@ -19,14 +20,24 @@ var Todo = mongoose.model('Todo', {
 //     console.log(e);
 // });
 
-var newTodo = new Todo({
-    text: 'Cook dinner',
-    completed: false,
-    completedAt: new Date()
+// var newTodo = new Todo({});
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved: ', doc)
+// }, (e) => {
+//     console.log(e);
+// });
+
+var User = mongoose.model('User', {
+    email: { type: String, trim: true, minLength: 1, required: true }
 });
 
-newTodo.save().then((doc) => {
-    console.log('Saved: ', doc)
-}, (e) => {
-    console.log(e);
+var newUser = new User({
+    email: 'ljmeyers80529@mail.fresnostate.edu'
+});
+
+newUser.save().then((doc) => {
+    console.log('Sabed: ', doc);
+}, (err) => {
+    console.log(err);
 });
