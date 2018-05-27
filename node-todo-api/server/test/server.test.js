0059025f0838abcda4f2ea209187b182a168a9ed
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-26 22:29:30 -0700

    Section 7, Lecture 76 - Testing GET /todos

--- node-todo-api/server/test/server.test.js
+++ node-todo-api/server/test/server.test.js
@@ -4,8 +4,22 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [{
+        text: 'Feed the cats'
+    },
+    {
+        text: 'Pet the kittens'
+    }
+];
+
+// beforeEach((done) => {
+//     Todo.remove({}).then(() => done());
+// });
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +37,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -41,7 +55,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
