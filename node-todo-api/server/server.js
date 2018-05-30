ec4259bbaf2af03a274329ab2d7fdea1e3eb4a01
ljmeyers80529@mail.fresnosate.edu <ljmeyers80529@mail.fresnostate.edu>
2018-05-30 13:37:26 -0700

    Section 7, Lecture 84 - PATCH (Updating) todos.

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,7 +1,8 @@
 const { ObjectID } = require('mongodb')
+const _ = require('lodash');
 
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 
 var { mongoose } = require('./db/mongoose.js');
@@ -57,6 +58,30 @@ app.delete('/todos/:id', (req, res) => {
         }
         res.send({ todo });
     }).catch((e) => res.status(404).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed'])
+
+    if (!ObjectID.isValid(id))
+        return res.status(404).send();
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((err) => {
+        res.status(404).send();
+    });
 });
 
 app.listen(3000, () => {
